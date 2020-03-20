6e8c9fa8030e11184d55411155baeb2d3d3cf72c
Dark Angel <jonathanyombo@gmail.com>
2020-03-20 11:30:30 +0200

    add all component to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MapComponent } from './map/map.component';
+import { VulgarisationComponent } from './vulgarisation/vulgarisation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthComponent
+    AuthComponent,
+    HeaderComponent,
+    FooterComponent,
+    MapComponent,
+    VulgarisationComponent
   ],
   imports: [
     BrowserModule,
