eb066d35522d464c5617def5c8c157d416fc3068
Dark Angel <jonathanyombo@gmail.com>
2020-03-20 11:31:03 +0200

    add a colon

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
   firebase: {
     apiKey: "AIzaSyBzH6IHUDQivrLGxCDO27biVl6v0kHHIIs",
     authDomain: "YOUR_AUTH_DOMAIN",
